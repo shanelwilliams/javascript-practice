2939ef778eeae50875cb331d6bd3363bc7f28f06
shanelwilliams <shanelwilliams2018@gmail.com>
2026-05-13 12:49:09 -0600

    First dice project

---
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,20 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1
+let randomDiceImage = "images/dice" + randomNumber1 + ".png"
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage)
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1
+let randomDiceImage2 = "images/dice" + randomNumber2 + ".png"
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2)
+
+let heading = document.querySelector("h1")
+if(randomNumber1 > randomNumber2) {
+    heading.innerHTML ="Player 1 Wins!"
+}
+else if(randomNumber1 === randomNumber2) {
+    heading.innerHTML = "Draw!"
+}
+else {
+    heading.innerHTML = "Player 2 Wins!"
+}
